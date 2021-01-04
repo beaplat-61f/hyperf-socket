c0285b5de18d72d5bbd37f6be1ac460877a25467
stephenshi(施振锋) <stephenshi@lexin.com>
2021-01-04 14:40:21 +0800

    user a

--- config/routes.php
+++ config/routes.php
@@ -14,6 +14,8 @@ use Hyperf\HttpServer\Router\Router;
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
 Router::addRoute('GET', '/connect', 'App\Controller\IndexController@connect');
 
+// 这是user a加的
+
 Router::get('/favicon.ico', function () {
     return '';
 });
